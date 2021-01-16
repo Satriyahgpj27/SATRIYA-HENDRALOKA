73b7a6195e4e6576b858c46b12ba510d55538e87
Satriyahgpj27 <Satriyahgpj27@users.noreply.github.com>
2021-01-16 22:04:25 +0700

    Revert "hasil praktikum LKDP no 7"
    This reverts commit fb52d940b7285356436effb0898dce01430ed1d8.

--- prak7/latihan 7-1.py
+++
@@ -1,7 +0,0 @@
-namaFile = input('Masukkan nama file : ')
-try :
-    file = open(namaFile, 'r')
-    print('Isi file ' + namaFile + ' adalah : ' )
-    print(file.read())
-except FileNotFoundError:
-    print('File tidak ditemukan')
--- prak7/praktikum 7-3.py
+++
@@ -1,8 +0,0 @@
-file = open('d:/data3.txt', 'r')
-sum = 0
-for data in file:
-    try:
-       sum = sum + int(data)
-    except ValueError:
-        print('invalid input')
-print(sum)
