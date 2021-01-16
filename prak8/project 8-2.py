a4732261ad862aaeee4c7971fda1ee0866709106
Satriyahgpj27 <Satriyahgpj27@users.noreply.github.com>
2021-01-16 22:05:04 +0700

    hasil praktikum LKPD 8

---
+++ prak8/project 8-2.py
@@ -0,0 +1,8 @@
+def dataStat(x):
+    a = sum(x)/len(x)
+    b = max(x)
+    c = min(x)
+    return [a, b, c]
+
+print(dataStat([5, 4, 6]))
+© 2021 GitHub, Inc.
---
+++ prak8/project 8-5.py
@@ -0,0 +1,6 @@
+sdef kuadrat(bil):
+    for i in range(len(bil)):
+        bil[i] **= 2
+    return bil
+
+print(kuadrat([2,5,7]))
